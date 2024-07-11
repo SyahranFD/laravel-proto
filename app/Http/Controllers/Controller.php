da45c6305e8bae2c5e8836354767758338c01c4a
SyahranFD <fadhilrafa1@gmail.com>
2024-07-11 22:46:02 +0700

    feat: [SF] add custom response in controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,39 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function resStoreData($data)
+    {
+        return response(['data' => $data], 201);
+    }
+
+    public function resUpdateData($data)
+    {
+        return response(['data' => $data], 201);
+    }
+
+    public function resInvalidLogin()
+    {
+        return response(['message' => 'Email or Password Is Invalid'], 409);
+    }
+
+    public function resUserNotFound()
+    {
+        return response(['message' => 'User Not Found'], 404);
+    }
+
+    public function resUserNotAdmin()
+    {
+        return response(['message' => 'User Not Admin'], 403);
+    }
+
+    public function resDataNotFound($data)
+    {
+        return response(['message' => $data.' Not Found'], 404);
+    }
+
+    public function resDataDeleted($data)
+    {
+        return response(['message' => $data.' Deleted'], 200);
+    }
 }
